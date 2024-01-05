c75fbb24fdcea611ad71514838b168909b9174d8
Nikesh Chaudhary <nikeshchaudhary52494@gmail.com>
2024-01-06 00:16:39 +0530

    footer changed

--- frontend/src/component/layout/Footer.js
+++ frontend/src/component/layout/Footer.js
@@ -1,38 +1,32 @@
 import React from "react";
-import { BiLogoPlayStore } from "react-icons/bi";
-import { FaAppStore } from "react-icons/fa";
 import Logo from "../images/byte.png"
+import { Link } from "react-router-dom";
 import {
   AiOutlineInstagram,
-  AiOutlineYoutube,
-  AiOutlineFacebook,
   AiOutlineTwitter,
+  AiOutlineLinkedin,
 } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div class="bg-slate-200 mt-40 dark:bg-slate-800">
-      <div class="flex max-w-5xl p-10 mx-auto justify-between items-center dark:text-white md:flex-row flex-col gap-10">
-        <div class="items-center text-center md:w-[33.33%] flex flex-col">
-          <h4 class="font-bold">DOWNLOAD OUR APP</h4>
-          <p>download App for Android And Ios mobile phone</p>
-          <BiLogoPlayStore />
-          <FaAppStore />
-        </div>
-        <div class="flex flex-col items-center md:w-[33.33%]">
+    <div className="mt-40 bg-slate-800">
+      <div className="flex max-w-5xl p-10 mx-auto justify-between items-center text-white md:flex-row flex-col gap-10 text-center">
+        <div className="flex flex-col items-center md:w-[70%]">
           <img className="w-40" src={Logo} alt="Byte Logo" />
-          <h1 class="text-5xl font-bold text-teal-400 m-2">ECOMMERCE</h1>
-          <p>High Quality is our first priority</p>
+          <h1 className="text-4xl font-bold m-2">ECOMMERCE</h1>
+          <p className="italic text-cyan-500 mb-2">Elevating your e-commerce experience with modern interactivity, showcasing how small changes can create a big impact on your lifestyle</p>
           <p>Copyright 2023 &copy; MeNikeshChaudhary</p>
         </div>
-        <div class="items-center flex flex-col md:w-[33.33%]">
-          <h4>FollowUs</h4>
-          <div class="flex gap-2 m-2">
-            <AiOutlineInstagram />
-            <AiOutlineFacebook />
-            <AiOutlineYoutube />
-            <AiOutlineTwitter />
-          </div>
+        <div className="flex  md:flex-col text-3xl gap-5">
+          <Link className="flex items-center" to="https://www.instagram.com/nikeshchaudhary52494/">
+            <AiOutlineInstagram /><span className="hidden text-xl md:block">Instagram</span>
+          </Link>
+          <Link className="flex items-center" to="https://www.linkedin.com/in/nikeshchaudhary52494/">
+            <AiOutlineLinkedin /><span className="hidden text-xl md:block">Linkedin</span>
+          </Link>
+          <Link className="flex items-center" to="https://twitter.com/nikesh52494">
+            <AiOutlineTwitter /><span className="hidden text-xl md:block">Twitter</span>
+          </Link>
         </div>
       </div>
     </div>
